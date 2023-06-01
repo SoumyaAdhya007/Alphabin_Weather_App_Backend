018ef6716a62a8824f04fe297411a2250532cfa6
Soumya Adhya <soumyaadhya007@gmail.com>
2023-06-01 09:56:48 +0530

    Backend

--- Model/user.model.js
+++ Model/user.model.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
+
+// Define the user schema
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true }, // User's email, required field
+  password: { type: String, required: true }, // User's password, required field
   preferences: {
-    temperatureUnit: { type: String, required: true },
-    languagePreference: { type: String, required: true },
+    unitPreference: { type: String, default: "M" }, // User's preferred unit, default is "M"
+    languagePreference: { type: String, default: "en" }, // User's preferred language, default is "en"
   },
-  preferred_locations: [{ type: String, default: [] }],
+  savedLocations: [
+    {
+      type: String,
+      required: true,
+      default: [],
+    },
+  ], // Array of saved locations, each location is of type String and required
 });
+
+// Create the user model
+const UserModel = mongoose.model("user", userSchema);
+
+// Export the user model
+module.exports = { UserModel };
